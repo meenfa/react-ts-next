93d38c371c059314c5e53534dd30549d87cc5081
ankitkarki27 <ankitkarki8088@gmail.com>
2026-06-25 02:03:04 +0545

    hook

---
+++ 06-hooks/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>this is hook part learning for react</h2>
+      <h2>This is hook part learnig for react learning</h2>
+    </div>
+  );
+}
+
+export default App;
