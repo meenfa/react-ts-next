1a7ffcb645f68c5ece9affc6b9593edb80f55510
ankitkarki27 <ankitkarki8088@gmail.com>
2026-07-06 14:39:05 +0545

    Context API update

--- 15-contextapi/src/context/ThemeContext.jsx
+++ 15-contextapi/src/context/ThemeContext.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useState } from "react";
 
-// 1. Creating context
+//1. Creating context
 export const ThemeDataContext = createContext();
 
 const ThemeContext = ({ children }) => {
+  //3.Consuming data using useContext hook
   const [theme, settheme] = useState("light");
 
   const toggletheme = () => {
