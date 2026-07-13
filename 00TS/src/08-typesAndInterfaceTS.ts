22b8525880cb3f31477cfc2cf44e660cf5916e34
ankitkarki27 <ankitkarki8088@gmail.com>
2026-07-13 14:36:17 +0545

    Types And Interface

--- 00TS/src/08-typesAndInterfaceTS.ts
+++ 00TS/src/08-typesAndInterfaceTS.ts
@@ -1,5 +1,7 @@
 // types n interface in TS
 
+// with classes we do more prefer interfaces than types
+// type is also called literals types
 type chiyaOrder={
     type:string;
     sugar:number;
@@ -32,7 +34,52 @@ interface CupSize {
     size:"small" | "large"
 }
  // to convert in interface for cupsize give value
-
 class chai implements CupSize{
     size :"small" | "large" ="large"
 }
+
+// | -> union
+// type Response2 = {ok:true}|{ok:false}
+// class myRes2 implements Response2{
+//     ok: boolean = true;
+// } 
+
+// UNION
+type TeaType = "masala"|"ginger" | " dudh" |"kalo"
+
+// t has teatype
+function orderChai(t:TeaType){
+    console.log(t);
+    
+}
+
+// Intersection =&
+type BaseChiya = {tealeaves:number}
+type Extra = {masala:number}
+
+type DudhChiya= BaseChiya & Extra
+
+const Onecup : DudhChiya = {
+    tealeaves:4,
+    masala:1
+}
+
+type User ={
+    username:string,
+    bio?:string
+}
+
+const u1:User = {username:"Ankit"}
+const u2:User = {username:"Ankit", bio:"meenfa.tech"}
+
+type Config ={
+    readonly appName:String,
+    version: number
+}
+
+const cfg:Config ={
+    appName : "TeamUp",
+    version : 1
+}
+
+// cfg.appName = "TeamUp  by ANkit"
